c5e22be3be7f6b681c1cad8c734be305e999dad5
Lily Calvetty <lili.calvettym@gmail.com>
2020-06-23 12:59:18 -0400

    Proyecto Inicial 23/06/20

---
+++ REPOSITORY/Interface/IPedido.vb
@@ -0,0 +1,13 @@
+﻿Imports ENTITY
+
+Public Interface IPedido
+    Function ListarPedidoDistribucion(listIdZona As List(Of Integer)) As List(Of VPedido_Dispatch)
+    Function ListarPedidoAsignadoAChofer(idChofer As Integer) As List(Of VPedido_BillingDispatch)
+    Function GuardarPedidoDeChofer(listIdPedido As List(Of Integer), idChofer As Integer) As Boolean
+    Function ListarDespachoXClienteDeChofer(idChofer As Integer) As List(Of RDespachoxCliente)
+    Function ListarDespachoXProductoDeChoferSalida(idChofer As Integer) As List(Of RDespachoXProducto)
+    Function ListarDespachoXProductoDeChofer(idChofer As Integer) As List(Of RDespachoXProducto)
+    Function VolverPedidoDistribucion(listIdPedido As List(Of Integer), idChofer As Integer) As Boolean
+    Function ListarDespachXNotaVentaDeChofer(idChofer As Integer, idPedido As Integer) As List(Of RDespachoNotaVenta)
+    Function ListarDespachXNotaVenta(idPedido As Integer) As List(Of RDespachoNotaVenta)
+End Interface
